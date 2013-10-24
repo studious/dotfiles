dd9d2ca1003e6616a5437310bb083c2add0fe3cb
Stuart Fraser <stuart@studiousonline.com>
2013-10-24 21:24:50 +1000

    Update GIT configurations

--- oh-my-zsh/custom/plugins/stufraser/stufraser.plugin.zsh
+++ oh-my-zsh/custom/plugins/stufraser/stufraser.plugin.zsh
@@ -2,3 +2,6 @@
 alias ghi='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[0m%x1b[32m%d%x1b[0m %s %x1b[30m(%an)%x1b[0m"'
 alias gs='git status'
 alias gdt='git describe --tags'
+alias gup='git smart-pull'
+alias gl='git smart-log'
+alias gm='git smart-merge'
